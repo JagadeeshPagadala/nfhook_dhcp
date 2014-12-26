82e68197c1e9e1c70c6fe4ed37e0319903ee1113
Jagadeesh Pagadala <jagadeesh.pagadala@nevisnetworks.com>
2014-12-26 12:16:46 +0530

    replaced printk with pr_debug

--- nfhook.c
+++ nfhook.c
@@ -17,7 +17,8 @@ unsigned int hook_func (unsigned int hooknum, struct sk_buff *skb,
 		const struct net_device *in,
 		const struct net_device *out, int (*okfn) (struct sk_buff *))
 {
-	printk("packet received");
+
+	pr_debug("packet received");
 	return NF_ACCEPT;
 }
 
