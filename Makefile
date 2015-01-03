41e689ddf7cb93f49fb0cf34e536a256ce6d2145
Jagadeesh Pagadala <jagadeesh.pagadala@nevisnetworks.com>
2015-01-03 17:12:14 +0530

    Added logic to find out DHCP ACK packet

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
-obj-m	:= nfhook.o
-KSRC	:=/lib/modules/`uname -r`/build
-PWD	:=`pwd`
-#CFLAGS-nfhook.o	:= -DPRINTK_DEBUG
-ccflags-y	:= -DPRINTK_DEBUG
+ifneq ($(KERNELRELEASE),)
+	obj-m	:= nfhook.o
+else 
+	KSRC	:=/lib/modules/`uname -r`/build
+	PWD	:=`pwd`
+	#CFLAGS-nfhook.o	:= -DPRINTK_DEBUG
+	ccflags-y	:= -DPRINTK_DEBUG
 all:
 	$(MAKE)  -C $(KSRC) M=$(PWD) modules
-	
 clean:
 	$(MAKE) -C $(KSRC) M=$(PWD) clean
--- nfhook.c
+++ nfhook.c
@@ -15,9 +15,9 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jagadeesh");
 
 
-/**	
-* 	Global variables	
-**/
+/**
+ * 	Global variables
+ */
 
 /* return '1' if packet is dhcp */
 static int is_udp(struct sk_buff *skb)
@@ -27,15 +27,18 @@ static int is_udp(struct sk_buff *skb)
 
 	ip_header = ip_hdr(skb);
 	/* IP protocol Number of UDP is IPPROTO_UDP(17). See linux/in.h*/
+	/* TODO: Check does evry packet contians IP hdr*/
 	if (ip_header->protocol == IPPROTO_UDP ) {
 		return 1;
 	}
 	return 0;
 }
 
-/* */
+/** 
+ *
+ */
 static int is_dhcp(struct sk_buff *skb)
-{	
+{
 	struct udphdr *udp_header;
 
 	udp_header = (struct udphdr *)udp_hdr(skb);
@@ -44,15 +47,16 @@ static int is_dhcp(struct sk_buff *skb)
 		(udp_header->source == htons(DHCP_CLIENT_PORT)) || \
 		(udp_header->dest == htons(DHCP_CLIENT_PORT))) {
 			return 1;
-
 		}
 	return 0;
 }
-/* */
+/**
+ *
+ */
 static int is_dhcp_ack(struct sk_buff *skb)
 {
 	/* Add logic to check is packet dhcp_ack */
-	struct udphdr *udp_header; 
+	struct udphdr *udp_header;
 	unsigned char *udp_data;
 	unsigned int type=0, type_offset=0;
 	u_int8_t op, htype;
@@ -70,17 +74,17 @@ static int is_dhcp_ack(struct sk_buff *skb)
 	return 0;
 }
 
-/*
-*	Netfilter Hook function
-*/
-unsigned int hook_func (unsigned int hooknum, struct sk_buff *skb, 
+/**
+ *	Netfilter Hook function
+ */
+unsigned int hook_func (unsigned int hooknum, struct sk_buff *skb,
 		const struct net_device *in,
 		const struct net_device *out, int (*okfn) (struct sk_buff *))
 {
 	int ret = 0;
 
 	pr_debug("%s:packet received \n",__FUNCTION__);
-#ifdef PRINTK_DEBUG	
+#ifdef PRINTK_DEBUG
 	printk("%s:packet received \n",__FUNCTION__);
 #endif
 	/* Check only Internet Protocol(IP) packets*/
@@ -96,7 +100,7 @@ unsigned int hook_func (unsigned int hooknum, struct sk_buff *skb,
 		return NF_ACCEPT;
 	}
 	pr_debug("%s: UDP packet RXd \n",__FUNCTION__);
-#ifdef PRINTK_DEBUG	
+#ifdef PRINTK_DEBUG
 	printk("%s: UDP packet RXd \n",__FUNCTION__);
 #endif
 	/* Check for DHCP packet */
@@ -105,9 +109,9 @@ unsigned int hook_func (unsigned int hooknum, struct sk_buff *skb,
 		return NF_ACCEPT;
 	}
 	pr_debug("%s:DHCP packet RXd \n",__FUNCTION__);
-#ifdef PRINTK_DEBUG	
+#ifdef PRINTK_DEBUG
 	printk("%s: DHCP packet RXd \n",__FUNCTION__);
-#endif 
+#endif
 	/* If packet RX is DHCP packet */
 	/* Check for DHCP ACK */
 	ret = is_dhcp_ack(skb);
@@ -115,31 +119,31 @@ unsigned int hook_func (unsigned int hooknum, struct sk_buff *skb,
 		return NF_ACCEPT;
 	}
 	pr_debug("%s:DHCP ACK packet RXd \n",__FUNCTION__);
-#ifdef PRINTK_DEBUG	
+#ifdef PRINTK_DEBUG
 	printk("%s: DHCP ACK packet RXd \n",__FUNCTION__);
-#endif 
+#endif
 	/* If we reach here, dhcp_ack packet */
 	return NF_ACCEPT;
 }
 
 
-static struct nf_hook_ops nfho = { 
+static struct nf_hook_ops nfho = {
 	.hook	= hook_func,
 	.owner	= THIS_MODULE,
 	.pf	= PF_INET, /* PF_BRIDGE is for bridge interface and  \
 							PF_INET for IPv4 */
-	/*TODO: 
+	/*TODO:
 	 * 1. If hook is registered at PRE_ROUTING then able to see DHCP Pkts
 	 * 2. If hook is registered at LOCAL_IN not able to see DHCP Packet.
-	 * Reason: May be LOCAL_IN hook is processed only if packet is having IP addr 
+	 * Reason: May be LOCAL_IN hook is processed only if packet is having IP addr
 	 * 	   same as Interface IP.
-	 *         If server is broadcasting DHCP ACK then, 
+	 *         If server is broadcasting DHCP ACK then,
 	 *         LOCAL_IN will not get the packet
-	*/
+	 */
 	.hooknum = NF_INET_PRE_ROUTING,
 	//.hooknum = NF_INET_LOCAL_IN,/* change to NF_INET_FORWARD to \
 		capture on arouter,  Refer netfiletr packet path diagram */
-	.priority = NF_IP_PRI_FIRST, 
+	.priority = NF_IP_PRI_FIRST,
 };
 
 static int nfhook_init(void)
@@ -147,12 +151,12 @@ static int nfhook_init(void)
 	int ret = 0;
 	ret = nf_register_hook(&nfho); /* It return 0 always */
 	
-	return 0;	
+	return 0;
 }
 
 static void nfhook_exit(void)
 {
-	nf_unregister_hook(&nfho);	
+	nf_unregister_hook(&nfho);
 }
 
 module_init(nfhook_init);
